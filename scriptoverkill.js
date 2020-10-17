fb278637e5f35fffd553ba215c91f1daa30c71de
imabotplayer <72506325+imabotplayer@users.noreply.github.com>
2020-10-17 09:36:20 +0200

    Create scriptoverkill.js

---
+++ scriptoverkill.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         CellsHub.ga Mouse Control
+// @version      1.0
+// @description  Left-click = Split, Right-click = Feed.
+// @author       Botplayer
+// @icon         http://bit.ly/2oT4wRk
+// @match        *://*.cellshub.ga/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+	"use strict";
+
+	const speed = 50;
+	let intervalID = null;
+
+	canvas.addEventListener("mousedown", ({button}) => {
+		if (button === 0) // left click
+			wsSend(UINT8_CACHE[17]);
+		if (button === 2) { // right click
+			wsSend(UINT8_CACHE[21]);
+			intervalID = setInterval(core.eject, speed);
+		}
+	});
+	addEventListener("mouseup", ({button}) => {
+		if (button === 2) {
+			clearInterval(intervalID);
+			intervalID = null;
+		}
+	});
+	canvas.addEventListener("mousewheel", () => {
+		canvas.dispatchEvent(new MouseEvent("mousemove", {
+			clientX: innerWidth / 2,
+			clientY: innerHeight / 2
+		}));
+	});
+	const prevent = event => event.preventDefault();
+	canvas.addEventListener("contextmenu", prevent);
+	canvas.addEventListener("drag", prevent);
+})();
